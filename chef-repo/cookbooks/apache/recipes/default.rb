c47425943d96c0b1ec625b5c3d7f8f44270fe9c4
Hariram Penumarthy <hariraminvrse@gmail.com>
2018-07-25 01:48:52 -0700

    apache playbook

--- chef-repo/cookbooks/apache/recipes/default.rb
+++ chef-repo/cookbooks/apache/recipes/default.rb
@@ -3,17 +3,43 @@
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
-package "apache2" do
+ package "apache2" do
     action :install
-end
-
-node.default["apache"]["indexfile"] = "index2.html"
-cookbook_file "/var/www/html/index.html" do
-    source node["apache"]["indexfile"]
-    mode "0777"
-end 
-
+ end
 
 service "apache2" do
     action [:enable , :start]
-end
+ end
+
+ execute "mv /etc/apache2/sites-enabled/000-default.conf /etc/apache2/sites-enabled/000-default.conf.disabled" do
+  notifies :restart, "service[apache2]"
+ end
+
+node["apache"]["sites"].each do |site_name, site_data|
+    document_root="/etc/apache2/sites-enabled/#{site_name}"
+
+ file "/etc/apache2/sites-enabled/#{site_name}.conf" do
+   source "custom.erb"
+    mode "0644"
+    variables (
+        :document_root => document_root,
+        :port => site_data["port"]
+    )
+
+    notifies :restart, "service[apache2]"
+ end
+
+ directory document_root do
+    mode "0777"
+    recursive true
+ end
+
+ template "#{document_root}/index.html" do
+    source "index.html.erb"
+    mode "0644"
+    variables (
+        :site_name => site_name,
+        :port => site_data["port"]
+    )
+    end
+end 
